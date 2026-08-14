f28d499f591898f07b9102a52680b216936354d3
Kartikaye <kartik.chat1212@gmail.com>
2026-08-14 21:30:53 +0530

    feat: add exploring food spots and dining out to About Me

--- sources/data/about.js
+++ sources/data/about.js
@@ -4,6 +4,6 @@ export default {
     paragraphs: [
         `Hey there! I’m Kartikaye. I’m currently pursuing my Master’s (M.Tech) in Computer Technology at IIT Delhi, after completing my bachelor’s in Electronics and Computer Engineering from Thapar Institute of Engineering and Technology, Patiala.`,
 
-        `Outside of academics, you’ll usually find me lifting at the gym, geeking out over horology and mechanical watch collecting, or planning my next travel destination. I’m also huge on pop culture: stand-up comedy specials, movies, binge-worthy TV series, and discovering new music. If there’s an intricate craft, a great story, or a solid laugh involved, I’m all in.`
+        `Outside of academics, you’ll usually find me lifting at the gym, geeking out over horology and mechanical watch collecting, planning my next travel destination, or exploring new food spots and eating out. I’m also huge on pop culture: stand-up comedy specials, movies, binge-worthy TV series, and discovering new music. If there’s an intricate craft, a great story, or a solid laugh involved, I’m all in.`
     ]
 }
